a10a1faac47c1ae6874ca61a020741dc10f5d749
rnnda-ux <rnandajr758@gmail.com>
2025-01-09 07:46:55 +0700

    Update perceptronapk.py

--- perceptronapk.py
+++ perceptronapk.py
@@ -4,8 +4,8 @@ from joblib import load
 import numpy as np
 
 # Memuat model dan scaler
-model_filename = 'D:/Machine Learning/akhir.py/supervised/mlp_fish_model.pkl'
-scaler_filename = 'D:/Machine Learning/akhir.py/supervised/scaler_fish_data.pkl'
+model_filename = 'mlp_fish_model.pkl'
+scaler_filename = 'scaler_fish_data.pkl'
 
 mlp_model = load(model_filename)
 scaler = load(scaler_filename)
